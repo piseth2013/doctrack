cd18247106d9a0f04fc9096b036a6bf764ba18b9
piseth2013 <128473317+piseth2013@users.noreply.github.com>
2025-05-30 14:54:05 +0700

    Fix authentication refresh and create positions table

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -131,6 +131,17 @@ const LoginPage: React.FC = () => {
             </div>
           </form>
 
+          <div className="mt-6">
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <div className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-sm">
+            
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
