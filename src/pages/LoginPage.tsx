a1efd3f7e45a4674ab32b64a9d6fc9fbaf65094b
piseth2013 <128473317+piseth2013@users.noreply.github.com>
2025-05-30 15:00:11 +0700

    Add default export to LoginPage component

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -71,7 +71,7 @@ const LoginPage: React.FC = () => {
   };
 
   if (user) {
-    return <Navigate to="/dashboard\" replace />;
+    return <Navigate to="/dashboard" replace />;
   }
 
   return (
@@ -171,3 +171,5 @@ const LoginPage: React.FC = () => {
     </div>
   );
 };
+
+export default LoginPage;
